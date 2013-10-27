d8f7e1a2112ad2521a075a64b241eae8b050b988
James Gatannah <james.gatannah@gmail.com>
2013-10-27 14:40:59 -0500

    Back to tweaking visual elements, hoping there's something minor I can change to make things start magically working

--- angulmine/app/js/directives.js
+++ angulmine/app/js/directives.js
@@ -10,11 +10,17 @@ angular.module('minesweepApp.directives', []).
 	};
     }]).
     directive('mineCell', function() {
-	function link(scope, element, attrs) {
-	    var msg = "Performing a Directive on a Cell.\nScope:";
+	function debug(scope, element, attrs) {
+	    var msg = "Performing a Directive on a Cell.";
+
+	    // The scope isn't interesting...except that I might have to
+	    // break down and extract the board state from it if I can't find
+	    // it under either element or attrs.
+	    /*msg += "\nScope:";
 	    for(var prop in scope.this) {
+		msg += "\n\t";
 		if(prop != '$new' && prop != 'constructor') {
-		    msg += "\n\t" + prop + ": ";
+		    msg += prop + ": ";
 		    if(typeof(scope[prop]) != 'undefined') {
 			msg += scope[prop];
 		    }
@@ -23,15 +29,19 @@ angular.module('minesweepApp.directives', []).
 		    }
 		}
 		else {
-		    msg += "\tconstructor: a big function for setting up a really nifty object";
+		    msg += "constructor: a big function for setting up a really nifty object";
 		}
-	    }
+	    }*/
 
 	    // This is pretty useless...it looks like it boils down to
-	    // [[HTMLSpanElement]]
+	    // [[HTMLSpanElement]] in my unit tests and an [[HTMLTableCellElement]] in practice.
+	    // Meaning that I just might be forced to get the value of the cell being
+	    // drawn here by digging into this part of the DOM.
 	    msg += "\n\nElement:\n" + element;
 
-	    // And this seems to be a duplication of the root scope.
+	    // Live, the available keys are [0, 1, 2, and 3]. Each with an Undefined value.
+	    // Q: Huh??!
+	    // A: Oh. attrs is still a string.
 	    msg += "\n\nAttributes:\n" + attrs + " (a " + typeof(attrs) + ")";
 	    for(var prop in attrs.mineCell) {
 		if(prop != '$new' && prop != 'constructor') {
@@ -45,11 +55,13 @@ angular.module('minesweepApp.directives', []).
 		}
 	    }
 	    console.log(msg);
+	}
 
+	function link(scope, element, attrs) {
 	    var cell = attrs.mineCell;
 
 	    // Update element based upon cell's state:
-	    var msg = "Directing: " + cell+ "\n";
+	    var msg = "Directing: " + cell + "\n";
 	    var result = ' ';
 
 	    for(var prop in cell) {
@@ -86,9 +98,11 @@ angular.module('minesweepApp.directives', []).
 		    }
 		}
 	    }
-	    msg = "Directed cell at (" + cell['x'] + ", " + cell['y'] + "): " + result + "\n" + msg;
+	    msg += "\nDirected cell at (" + cell['x'] + ", " + cell['y'] + "): " + result + "\n" + msg;
 	    console.log(msg);
 	    element.text(result);
+
+	    return result;
 	}
 	return { link: link };
     }).
@@ -98,15 +112,23 @@ angular.module('minesweepApp.directives', []).
 	    var timeoutId;
 
 	    function updateTime() {
-		var display = 0;
+		// TODO: Handling the timer this way seems to totally obsolete the
+		// need for the time controller.
+		// FIXME: Really need to completely decouple this from the scope...
+		// Actually, the real problem is concerns that aren't separate.
+		/*var display = 0;
 		if(scope.started) {
 		    var current = new Date();
 		    display = Math.floor ((current - scope.started) / 1000);
-		}
+		}*/
+		//var display = scope.Time();
 		element.text(display);
 	    }
 
 	    function scheduleUpdate() {
+		// Note that doing this means we don't need to bother with the
+		// time controller.
+
 		// Save the timeoutId for canceling
 		timeoutId = $timeout(function() {
 		    updateTime();
