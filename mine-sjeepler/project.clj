5dd67f900ecfedd9ea9f1dbfaceedd7672dad6c0
James Gatannah <james.gatannah@gmail.com>
2016-02-27 00:45:46 -0600

    Bump dependency versions for improved workflow
    Mainly figwheel
    
    Because I'm curious how this looks in clojurescript and om next.
    
    It's total overkill, and I'm also curious how it looks under plain
    clojurescript these days, but one thing at a time. It's not like I need
    to optimize this thing for serious performance.

--- mine-sjeepler/project.clj
+++ mine-sjeepler/project.clj
@@ -1,16 +1,16 @@
 (defproject mine-sjeepler "0.1.0-SNAPSHOT"
-  :description "Minesweeper clone, to show off my mad javascript skillz"
+  :description "Minesweeper clone, to experiment w/ om-next"
   :url "http://example.com/FIXME"
-  :dependencies [[compojure "1.1.5"]
-                 [enlive "1.1.4"]
-                 [org.clojure/clojure "1.5.1"]
-                 [org.clojure/clojurescript "0.0-1934"]
-                 [ring "1.1.8"]
-                 [spyscope "0.1.3"]]
-  :plugins []
-  :hooks [leiningen.cljsbuild]
-  :source-paths ["src/clj"]
-  :cljsbuild { 
+  :dependencies [[compojure "1.4.0"]  ; TODO: Probably don't need
+                 [devcards "0.2.1-6"]
+                 [enlive "1.1.6"]  ; TODO: also probably don't need
+                 [figwheel-sidecar "0.5.0-6" :scope "test"]
+                 [org.clojure/clojure "1.8.0"]
+                 [org.clojure/clojurescript "1.7.228"]
+                 [org.omcljs/om "1.0.0-alpha22"]
+                 [ring "1.4.0"]  ; another good deletion candidate
+                 [spyscope "0.1.5"]]  ; probably not a lot of use for this one, either
+  :cljsbuild {
     :builds {
       :dev {
         :source-paths ["src/cljs"]
@@ -31,11 +31,12 @@
                    :pretty-print true}
         :jar true}}}
   :main mine-sjeepler.core
-  :profiles {:dev {:plugins [[com.cemerick/austin "0.1.1"]
-                             [lein-cljsbuild "0.3.2"]
-                             [lein-ring "0.8.3"]]
+  :profiles {:dev {:plugins [#_[com.cemerick/austin "0.1.1"]
+                             #_[lein-cljsbuild "0.3.2"]
+                             #_[lein-ring "0.8.3"]]
                    :source-paths ["dev"]
-                   :dependencies [[org.clojure/tools.namespace "0.2.3"]
-                                  [org.clojure/java.classpath "0.2.0"]]}}
-  :ring {:handler scrabbletris.server/app})
-
+                   :dependencies [[org.clojure/tools.namespace "0.2.10"]
+                                  [org.clojure/java.classpath "0.2.3"]]}}
+  :plugins []
+  :ring {:handler scrabbletris.server/app}
+  :source-paths ["src/clj"])
